f453340217e69e7f684abbd95470eeb461e62976
Yunseok Lee <yser.lee@samsung.com>
2017-02-27 16:00:23 +0900

    Update test.js

--- test.js
+++ test.js
@@ -4,72 +4,70 @@ function CWE_398() { // IDENTICAL_BRANCHES
     if (x >= 0) {
         y = x;
     } else {
-        y = -x;
+        y = x;
     }
 }
 
 function CWE_476() { // NULL_POINTER
-    var obj = {};
+    var obj;
     var y = obj.x;
     console.log(y);
 }
 
-function CWE_480(obj) { // BAD_BITWISE_OPERATOR
-    //var obj = {};
-    if (obj && obj.prop) {
+function CWE_480() { // BAD_BITWISE_OPERATOR
+    var obj = null;
+    if (obj & obj.prop) {
         console.log(obj.prop);
     }
 }
 
-function CWE_480_481(x) { // BAD_ASSIGN_IN_CONDITION
-    //var x = -1;
-    if (x === -1) console.log('Error!', x);
+function CWE_480_481() { // BAD_ASSIGN_IN_CONDITION
+    var x = -1;
+    if (x = -1) console.log('Error!', x);
 }
 
 function CWE_482_665() { // UNUSED_EXPR
-    //this.foo;
+    this.foo;
 }
 
 function CWE_484() { // SWITCH_CASE_FALL_THROUGH
     var x;
     switch (x) {
-    case '1': console.log('Do one thing'); break;
+    case '1': console.log('Do one thing');
     case '2': console.log('Do another thing');
     }
 }
 
-function CWE_489_569_570_571(x) { // CONSTANT_CONDITION
-    //var x = "foo";
+function CWE_489_569_570_571() { // CONSTANT_CONDITION
+    var x = "foo";
     if (x === "bar") {
         console.log('Do something');
     }
 }
 
 function CWE_561() { // UNREACHABLE_CODE
-    //return;
+    return;
 
     var bar = 1;
     console.log(bar);
 }
 
 function CWE_563(url) { // UNUSED_VAR_ASSIGN
-//     var foo = url + "?query=deepscan";
-//     foo = url.replace(/\s/gi, "");
-//     console.log(foo);
-    
-//     return foo;
+    var foo = url + "?query=deepscan";
+    foo = url.replace(/\s/gi, "");
+    console.log(foo);
 }
 
 function CWE_628() { // TOO_MANY_ARGS
     function add(x, y) {
         return x + y;
     }
-    var sum = add(1, 2);
+    var sum = add(1, 2, 3);
     console.log(sum);
 }
 
 function CWE_670(x) { // STRAY_SEMICOLON
-    while (++x <= 10)
+    while (++x <= 10) ;
     {
         sum += x;
     }
